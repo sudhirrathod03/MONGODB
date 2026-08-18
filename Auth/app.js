1f80ea9bef430523755a2f37865c1bfe817d8637
Sudhir Rathod <125038605+sudhirrathod03@users.noreply.github.com>
2026-08-18 22:59:53 +0530

    Validate email and password in login route
    Add validation for email and password fields in login.

--- Auth/app.js
+++ Auth/app.js
@@ -31,6 +31,9 @@ app.post("/register", async (req, res) => {
 app.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
+   if (!email || !password) {
+      return res.status(400).json({ message: "All fields are required!" });
+    }
     const user = await User.findOne({ email });
     if (!user) {
       return res.status(404).json({ message: "User does not exist!" });
